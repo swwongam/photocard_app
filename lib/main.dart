c3842bf16bcb4d7ac9d7800885a28c046e5972a8
Davis Wong <126454957+swwongam@users.noreply.github.com>
2025-07-02 14:38:15 -0400

    deleted impart hive.dart line in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'models/photocard.dart';
 
